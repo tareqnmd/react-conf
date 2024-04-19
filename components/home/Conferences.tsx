768fb154b158afbbef3694e682740ad678c65baa
Tareq <tareqnmd@gmail.com>
2024-04-20 02:47:44 +0600

    conference design updated

--- components/home/Conferences.tsx
+++ components/home/Conferences.tsx
@@ -36,7 +36,10 @@ const getConferenceData = async () => {
 const Conferences = async () => {
 	const conferences = await getConferenceData();
 	return (
-		<div className="container">
+		<div className="container grid py-[40px] lg:py-[100px] gap-[20px] lg:gap-[40px]">
+			<h3 className="text-[#0A142F] font-[700] text-[24px] lg:text-[48px] text-center">
+				Conferences
+			</h3>
 			<div className="conference-timeline pl-[16px] lg:pl-[0px]">
 				{conferences?.map((conference: ConferenceCardType, index: number) => (
 					<ConferenceTimeline
