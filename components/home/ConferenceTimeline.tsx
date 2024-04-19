7774fce66fa929fbdab7178996a0539a2f060a46
Tareq <tareqnmd@gmail.com>
2024-04-20 01:01:50 +0600

    initial large timelines added

--- components/home/ConferenceTimeline.tsx
+++ components/home/ConferenceTimeline.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/lib/util';
 import ConferenceCard from './ConferenceCard';
 import { ConferenceCardType } from './Conferences';
 
@@ -9,36 +10,34 @@ const ConferenceTimeline = ({
 	type: string;
 }) => {
 	return (
-		<>
-			<div className="flex flex-row-reverse md:contents">
-				<div className="col-start-1 col-end-5 ml-auto ">
-					<ConferenceCard conference={conference} />
-				</div>
-				<div className="col-start-5 col-end-6 md:mx-auto relative mr-10">
-					<div className="h-full w-6 flex items-center justify-center">
-						<div className="h-full w-1 bg-blue-800 pointer-events-none"></div>
-					</div>
-					<div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"></div>
-				</div>
+		<div className="flex flex-col md:grid grid-cols-12 mx-auto">
+			<div
+				className={cn(
+					'',
+					type === 'left'
+						? 'col-start-1 col-end-6 order-1'
+						: 'col-start-7 col-end-12 order-3'
+				)}
+			>
+				<ConferenceCard conference={conference} />
 			</div>
-			{conference?.startDate}
-			<div className="flex md:contents">
-				<div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
-					<h2 className="text-[#000]"></h2>
-					<div className="h-full w-6 flex items-center justify-center">
-						<div className="h-full w-1 bg-blue-800 pointer-events-none"></div>
-					</div>
-					<div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"></div>
+			<div className="col-start-6 col-end-7 md:mx-auto relative order-2">
+				<div className="h-full w-6 flex items-center justify-center">
+					<div className="h-full w-1 bg-[#cdcdcd] pointer-events-none"></div>
 				</div>
-				<div className="bg-blue-500 col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md">
-					<h3 className="font-semibold text-lg mb-1">Lorem ipsum</h3>
-					<p className="leading-tight text-justify">
-						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae,
-						facilis.
-					</p>
-				</div>
+				<div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-[#cdcdcd] shadow"></div>
 			</div>
-		</>
+			<div
+				className={cn(
+					'grid items-center',
+					type === 'left'
+						? 'col-start-7 col-end-12 justify-start order-3'
+						: 'col-start-1 col-end-6 justify-end order-1'
+				)}
+			>
+				{conference?.startDate}
+			</div>
+		</div>
 	);
 };
 
