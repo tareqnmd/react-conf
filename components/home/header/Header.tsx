18293201a07d1e0983018eebd9e29558978ec601
Tareq <tareqnmd@gmail.com>
2024-04-20 13:08:47 +0600

    header snake line added

--- components/home/header/Header.tsx
+++ components/home/header/Header.tsx
@@ -1,6 +1,7 @@
 import headerImg2 from '@/assets/images/header_img_2.png';
 import LineCircle from '@/components/icons/LineCircle';
 import LineCircleHalf from '@/components/icons/LineCircleHalf';
+import SmallSnakeLine from '@/components/icons/SmallSnakeLine';
 import SnakeLine from '@/components/icons/SnakeLine';
 import Image from 'next/image';
 import BuyTickets from './BuyTickets';
@@ -12,8 +13,11 @@ const Header = () => {
 		<div className="relative overflow-hidden py-[56px] lg:py-[100px]">
 			<div className="container max-w-[1550px] p-[1.5rem] lg:p-[1rem]">
 				<div className="grid grid-cols-1 lg:grid-cols-12 gap-[32px] lg:gap-[40px] place-items-end relative">
-					<div className="absolute top-0 2xl:right-0">
+					<div className="max-lg:hidden absolute top-0 right-[-100px] xl:right-[-30px] 2xl:right-0">
 						<SnakeLine />
+					</div>
+					<div className="lg:hidden absolute top-0 left-0">
+						<SmallSnakeLine />
 					</div>
 					<div className="col-span-1 lg:col-span-7 grid gap-[30px] z-[1]">
 						<HeaderTitle />
