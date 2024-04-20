cbe68155b3be4a2426c456dc9eed78ad48671ce2
Tareq <tareqnmd@gmail.com>
2024-04-20 19:32:25 +0600

    update

--- components/conference/Conference.tsx
+++ components/conference/Conference.tsx
@@ -11,20 +11,6 @@ export type ConferenceType = {
 };
 const Conference = ({ conference }: { conference: ConferenceType }) => {
 	const { id, activeTab, name, slogan, conferenceTabs } = conference || {};
-	const handleDragStart = (event: any) => {
-		event.dataTransfer.setData('application/my-app', event.target.id);
-		event.dataTransfer.effectAllowed = 'move';
-	};
-	const handleDragOver = (event: any) => {
-		event.preventDefault();
-		event.dataTransfer.dropEffect = 'move';
-	};
-	const handleDrop = (event: any) => {
-		event.preventDefault();
-		// const draggable_item = event.dataTransfer.getData('application/my-app');
-		// const drop_target = document.getElementById('drop_container');
-		// drop_target.appendChild(document.getElementById(draggable_item));
-	};
 	return (
 		<div className="container grid gap-[40px] lg:gap-[52px] my-[20px] pt-[88px] lg:pt-[110px] lg:my-[60px]">
 			<div className="grid gap-[12px] lg:gap-[16px]">
@@ -36,19 +22,10 @@ const Conference = ({ conference }: { conference: ConferenceType }) => {
 				</p>
 			</div>
 			<div className="grid grid-cols-12 lg:gap-[48px] place-items-start">
-				<div
-					onDragOver={handleDragOver}
-					onDrop={handleDrop}
-					id="drop_container"
-					className="col-span-12 lg:col-span-4 grid gap-[24px] lg:gap-[32px] w-full"
-				>
+				<div className="col-span-12 lg:col-span-4 grid gap-[24px] lg:gap-[32px] w-full">
 					{conferenceTabs?.map((tab: string) => (
 						<>
-							<Link
-								draggable="true"
-								onDragStart={handleDragStart}
-								href={`/conference/${id}?tab=${tab}`}
-							>
+							<Link href={`/conference/${id}?tab=${tab}`}>
 								<ConferenceTab
 									active={activeTab === tab}
 									title={tab}
